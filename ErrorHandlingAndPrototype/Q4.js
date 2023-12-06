8dc61bc9fc74a545f030429eb5656414a7164153
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-12-06 15:58:30 +0530

    Error Handling and Prototype

---
+++ ErrorHandlingAndPrototype/Q4.js
@@ -0,0 +1,23 @@
+//4. Employee Class Challenge.
+
+//Create a class called Employee with three properties: name, position, and salary. The class should have a method called getSalary that returns the employee's salary. Instantiate an instance of the Employee class and call the getSalary method.
+
+//Expected Output:
+console.log(employee1.getSalary()); 
+
+
+class Employee {
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.salary = salary;
+  }
+
+  getSalary() {
+    return this.salary;
+  }
+}
+
+const employee1 = new Employee("Prabir Kumar", "Software Engineer", 80000);
+
+console.log(employee1.getSalary()); 
